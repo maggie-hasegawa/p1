8b04f9397cb5dbf8dcd8d0b704702bf94c3e520a
Maggie Hasegawa <mcape904@gmail.com>
2014-09-18 23:10:09 -0400

    Added index file and related bootstrap files.

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Maggie Hasegawa P1 - DWA15 Portfolio</title>
+	<link rel='stylesheet' href="css/bootstrap.min.css" type='text/css'>
+	<script src="js/bootstrap.min.js"></script>
+
+	<style>
+
+	#links {
+	  float:left;
+	  padding: 8px;
+	}
+
+	#img {
+	  float: left;
+	  padding-left: 20px;
+	  padding-top: 30px;
+	}
+
+	#content {
+	  clear: both;
+	}
+
+	</style>
+
+</head>	
+<body>
+	<div class="container">
+		<h1>Maggie Hasegawa <br> Dynamic Web Apps Portfolio</h1>
+
+		<div id="links">
+			<h3>Project 2 Title</h3>
+			View: [link]
+			Github: [link]
+			
+			<h3>Project 3 Title</h3>
+			View: [link]
+			Github: [link]
+			
+			<h3>Project 4 Title</h3>
+			View: [link]
+			Github: [link]
+		</div>
+
+		<div id="img"><img src="img/intro_pic.jpg" alt="Maggie Hasegawa"></div>
+
+      <div id="content">
+      	<hr>
+		<h2>In 2+ sentences describe who you are and what you do.</h2>
+		<p>Hi! My name is Maggie and I currently work as the Digital Library Application Developer at Princeton Theological Seminary in Princeton, NJ. I'm a Librarian by trade, so this job is the perfect mix of programming and librarianship.</p>
+		<p>Outside of work, I live with my husband and kitty and watch much too much Doctor Who!</p>
+
+		<h2>Why are you taking this course?</h2>
+		<p>After working as a library assistant for a few years, despite having no real programming experience I decided I'd rather work in web development in some way and signed up for the Web Technologies Certificate program to try to gain the skills needed to make the career switch. After the first course in the program, I was hired in my current position as application developer, so now I'm taking this course to broaden the skills I've picked up on the job.</p>
+
+		<h2>What previous experience do you have with programming?</h2>
+		<p>As part of the Web Technologies Certificate program at Harvard, I've taken the Fundamentals of Website Development and Web Development Using XML courses. In my job at the Seminary, I work almost exclusively in XQuery, but with some HTML, CSS, Javascript, and XSLT thrown in. Our applications are developed using MarkLogic Server and the MVC architecture.</p>
+
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p>I'm working on a Mac.</p>
+
+		<h2>Anything else we should know?</h2>
+		<p>Since I've started taking courses through the Harvard Extension School, I've gotten married and changed my name. My registration appears as Margarete Capewell.</p>
+	  </div>
+	</div>
+</body>
+</html>
